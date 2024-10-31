0c9a759aed53e39231d9dad5b1e8426643c6efff
pandavas89 <pandavas89@gmail.com>
2024-10-31 16:00:12 +0900

    👷 CI: lambda 배포 준비

--- lambda_function.py
+++ lambda_function.py
@@ -1,24 +1,9 @@
-import boto3
-import urllib.parse
-import datetime
-from pytz import timezone
-
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('notion-daily-quote-subscription')
+from daily_quote import main
 
 def lambda_handler(event, context):
-    body = urllib.parse.parse_qs(event['body'])
-    
-    today = datetime.datetime.now().replace(tzinfo=timezone('Asia/Seoul')).date()
-    item = {
-        'quote_id': int(body['quote_id']),
-        'start_date': (today + datetime.timedelta(days=1)).strftime('%Y-%m-%d'),
-        'database_id': body['database_id']
-    }
-    
-    response = table.put_item(Item=item)
-    
+    main()
+
     return {
         'statusCode': 200,
-        'body': '데이터가 성공적으로 업데이트되었습니다'
+        'body': '람다 함수가 성공적으로 실행되었습니다'
     }
