04131132d5878162607934e957557af7d4c42cf0
pandavas89 <pandavas89@gmail.com>
2024-10-31 15:59:27 +0900

    🎨 REFAC: daily_quote 내 main 함수 추가

--- daily_quote.py
+++ daily_quote.py
@@ -2,39 +2,45 @@
 app that inserts designated api based on notion api
 '''
 import datetime
+from zoneinfo import ZoneInfo
 
 
 from dynamodb import DynamoDB
 from s3 import S3
 from notion import Notion
 
+def main():
+    dynamodb = DynamoDB()
+    s3 = S3()w
+    notion = Notion()
 
-dynamodb = DynamoDB()
-s3 = S3()
-notion = Notion()
+    # 명언 데이터베이스 정보 취득
+    quote_datas = dynamodb.read_datas()
+    utc_now = datetime.datetime.now(ZoneInfo("UTC"))
+    print(f"Now is {utc_now.strftime('%Y-%m-%d %H:%M:%S')}")
+    today = utc_now.astimezone(ZoneInfo("Asia/Seoul")).date()
+    print(f"Today is {today}")
 
-# 명언 데이터베이스 정보 취득
-quote_datas = dynamodb.read_datas()
-today = datetime.datetime.today()
-print(f"Today is {today.strftime('%Y-%m-%d %H:%M:%S')}")
-today = today.date()
+    # 명언 데이터베이스별 처리
+    for quote in quote_datas:
+        status, df = s3.read_quote(quote['bucket'], quote['file'])
+        quote_modular = df.shape[0]
+        subscriptions = dynamodb.read_subscription(quote['quote_id'], quote_modular, today)
+        for mod, sub_list in subscriptions.items():
+            # 오늘의 인용구 추출
+            today_quote = df.iloc[mod, :]
+            # 개별 구독에 대해 처리
+            for database_id in sub_list:
+                # notion.daily_customization(database_id)
+                page_found, block_id = notion.find_today_page(database_id)
+                # 당일 문서가 발견된 경우
+                if page_found:
+                    notion.replace_quote(block_id, today_quote['Quote'])
+                    notion.insert_quote(block_id, today_quote['Verse'])
+                else:
+                    print('block not found')
 
-# 명언 데이터베이스별 처리
-for quote in quote_datas:
-    status, df = s3.read_quote(quote['bucket'], quote['file'])
-    quote_modular = df.shape[0]
-    subscriptions = dynamodb.read_subscription(quote['quote_id'], quote_modular, today)
-    for mod, sub_list in subscriptions.items():
-        # 오늘의 인용구 추출
-        today_quote = df.iloc[mod, :]
-        # 개별 구독에 대해 처리
-        for database_id in sub_list:
-            notion.daily_customization(database_id)
-            page_found, block_id = notion.find_today_page(database_id)
-            # 당일 문서가 발견된 경우
-            if page_found:
-                notion.replace_quote(block_id, today_quote['Quote'])
-                notion.insert_quote(block_id, today_quote['Verse'])
-            else:
-                print('block not found')
+
+if __name__ == "__main__":
+    main()
         
