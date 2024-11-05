64874b2f3e03fc6e0ece760bcadecfb3a23baf6d
pandavas89 <pandavas89@gmail.com>
2024-11-05 16:23:26 +0900

    🐛 FIX: serverless bug fix

--- api/quote.py
+++ api/quote.py
@@ -1,2 +1,10 @@
-from ..daily_quote import main
+import sys
+import os
+
+# 부모 디렉토리 경로 추가
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# 부모 디렉토리에 있는 daily_quote.py 임포트
+from daily_quote import main
+
 main()
